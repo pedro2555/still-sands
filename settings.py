4432a83cd7df1d14229e4820cf1d4752fe54abc6
Pedro Rodrigues <prodrigues1990@gmail.com>
2016-11-19 23:05:50 +0000

    Added mongodb configs

--- settings.py
+++ settings.py
@@ -1,1 +1,11 @@
+import os
+
+# We want to seamlessy run our API both locally and on Heroku. If running on
+# Heroku, sensible DB connection settings are stored in environment variables.
+MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
+MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME', '')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', '')
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'still-sands-21077')
+
 DOMAIN = {'products':{}}
